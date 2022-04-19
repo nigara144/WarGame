633fe6b783b35442675ea95bc416bf4b69ac1b09
Nigar Azadaliyev <nigar.azadaliyeva@gmail.com>
2022-04-19 14:41:33 +0200

    Initial Commit

---
+++ WarGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarGame
+//
+//  Created by user216966 on 4/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
