fa307a19b3e3ab61b90b943253cd93eea06ee144
Nigar Azadaliyev <nigar.azadaliyeva@gmail.com>
2022-04-21 15:37:59 +0200

    # final changes: added timer, play button

--- WarGame/ViewController.swift
+++ WarGame/ViewController.swift
@@ -7,34 +7,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerScoreLabel: UILabel!
     @IBOutlet weak var computerScoreLabel: UILabel!
     @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
     
     var playerScore: Int = 0
     var computerScore: Int = 0
+    var roundNumber: Int = 0
+    var timer: Timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        }
+    
+    @objc func play() {
+        updateRoundNumber()
+        let player = getRandomCards()
+        let computer = getRandomCards()
+        
+        updateCards(player: player, computer: computer)
+        
+        updatePlayersScores(player: player, computer: computer)
+        
+        if(roundNumber == 15){
+            playButton.isHidden = false
+            timer.invalidate()
+        }
     }
     
-    @IBAction func deal() {
-        let player = getRandomCardNumber()
-        let computer = getRandomCardNumber()
-        
-        updateCardImages(player: player, computer: computer)
-        
-        updateScores(player: player, computer: computer)
+    @IBAction func startGame() {
+        playButton.isHidden = true
+        resetGame()
+        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(self.play), userInfo: nil, repeats: true)
     }
     
-    func getRandomCardNumber() -> Int {
+    func updateCards(player: Int, computer: Int) {
+        playerCardImage.image = UIImage(named: "c-\(player)")
+        computerCardImage.image = UIImage(named: "c-\(computer)")
+    }
+    
+    func getRandomCards() -> Int {
         return Int.random(in: 2...14)
     }
     
-    func updateCardImages(player: Int, computer: Int) {
-        playerCardImage.image = UIImage(named: "card-\(player)")
-        computerCardImage.image = UIImage(named: "card-\(computer)")
-    }
-    
-    func updateScores(player: Int, computer: Int) {
+    func updatePlayersScores(player: Int, computer: Int) {
         if (player > computer) {
             playerScore += 1
             playerScoreLabel.text = String(playerScore)
@@ -42,5 +56,19 @@ class ViewController: UIViewController {
             computerScore += 1
             computerScoreLabel.text = String(computerScore)
         }
+    }
+    
+    func updateRoundNumber() {
+        roundNumber += 1
+        roundLabel.text = "Round: \(roundNumber)/15"
+    }
+    
+    func resetGame() {
+        roundNumber = 0
+        playerScore = 0
+        computerScore = 0
+        playerScoreLabel.text = String(playerScore)
+        computerScoreLabel.text = String(computerScore)
+        roundLabel.text = "Round: \(roundNumber)/15"
     }
 }
